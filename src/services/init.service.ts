3306d3f1e3adc591ea9506207f58799188161d13
Jonathan A <jonathanxl1@hotmail.com>
2025-01-03 14:16:11 -0500

    feat: Create base service consume endpoint

---
+++ src/services/init.service.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export function setBearerToken(token: string) {
+  api.interceptors.request.use(
+    (config) => {
+      config.headers["Authorization"] = `Bearer ${token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+}

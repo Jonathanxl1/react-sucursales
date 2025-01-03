4c65b88d87fbf35ee355dffd56ace14fbf657f65
Jonathan A <jonathanxl1@hotmail.com>
2025-01-03 14:24:49 -0500

    refactor: Added component manage routes to pages

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from "react";
 import Navbar from "../components/NavBar";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 interface Props extends PropsWithChildren {
   navbar: JSX.Element;
@@ -11,23 +12,22 @@ interface Props extends PropsWithChildren {
 function Layout({ navbar, aside, main, footer, ...others }: Partial<Props>) {
   return (
     <>
-      <div className="w-full min-h-[600px] h-auto bg-slate-400 ">
-        <nav className="w-full min-h-[40px] bg-red-500">
+      <BrowserRouter>
+        <div className="w-full min-h-[600px] h-auto bg-slate-200 ">
+          <nav className="w-full min-h-[40px] bg-gray-700">
           {navbar ? navbar : <Navbar />}
         </nav>
 
         <main className="w-full h-full min-h-[800px] flex">
-          <aside className="sm:hidden md:block md:w-2/12 md:h-auto  bg-blue-400">
-            {aside}
-          </aside>
-          <div className="sm:w-full sm:h-auto md:w-10/12 bg-green-400">
-            {main}
-          </div>
+            <Routes>
+              <Route path="/" element={<Login />} />
+            </Routes>
         </main>
         <footer className="relative left-0 bottom-0 w-full min-h-[200px] bg-yellow-600">
           {footer}
         </footer>
       </div>
+      </BrowserRouter>
     </>
   );
 }

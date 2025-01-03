4016f22fbc0f8ae000268d06b0f83ed2b1d0241a
Jonathan A <jonathanxl1@hotmail.com>
2025-01-03 14:27:42 -0500

    refactor: Personlize Navbar with link from react router

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,19 @@
+import { NavLink } from "react-router";
+
 function Navbar() {
   return (
     <>
-      <div className="w-full h-auto flex items-center justify-between p-2">
-        <span className="bg-purple-400">Logo</span>
+      <div className="w-full h-auto flex items-center justify-between p-2 text-gray-300">
+        <NavLink to="/">
+          <span className="">Sucursal Mapper</span>
+        </NavLink>
         <ul className="hidden md:flex list-none space-x-5">
-          <li>Links</li>
-          <li>Links</li>
+          <li>
+            <NavLink to="/delegations">Delegation</NavLink>
+          </li>
+          <NavLink to="/me">
+            <li>About me</li>
+          </NavLink>
         </ul>
       </div>
     </>

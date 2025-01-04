839c84483149c43adeabeb3a4b0444ce2f6b874c
Jonathan A <jonathanxl1@hotmail.com>
2025-01-04 00:58:48 -0500

    fix:Validate session - set link route page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,29 @@
-import { NavLink } from "react-router";
+import { NavLink, useLocation, useNavigate } from "react-router";
+import {
+  cleanBeareToken,
+  getBearerToken,
+} from "../storage/localStorage.storage";
+import { useEffect, useState } from "react";
 
 function Navbar() {
+  let [logged, SetLogged] = useState(false);
+  let location = useLocation();
+  let navigate = useNavigate();
+
+  function checkLogin() {
+    let token = getBearerToken();
+    SetLogged(() => token == null);
+  }
+
+  function closeSession() {
+    cleanBeareToken();
+    navigate("/");
+  }
+
+  useEffect(() => {
+    checkLogin();
+  }, [location]);
+
   return (
     <>
       <div className="w-full h-auto flex items-center justify-between p-2 text-gray-300">
@@ -8,12 +31,20 @@ function Navbar() {
           <span className="">Sucursal Mapper</span>
         </NavLink>
         <ul className="hidden md:flex list-none space-x-5">
-          <li>
-            <NavLink to="/delegations">Delegation</NavLink>
-          </li>
-          <NavLink to="/me">
-            <li>About me</li>
-          </NavLink>
+          {logged ? (
+            <>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/aboutme">About me</NavLink>
+              </li>
+              <li onClick={closeSession}>Close Session</li>
+            </>
+          )}
         </ul>
       </div>
     </>

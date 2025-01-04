1d960b1e405655bd0d3821ae2b98e39c99588492
Jonathan A <jonathanxl1@hotmail.com>
2025-01-04 00:49:11 -0500

    fix: Set token reload page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import "./App.css";
 import Layout from "./layouts";
+import { getBearerToken } from "./storage/localStorage.storage";
+import { setAuthorizationToken } from "./services/init.service";
 
 function App() {
+  useEffect(() => {
+    let token = getBearerToken();
+    if (token) {
+      setAuthorizationToken(token);
+    }
+  }, []);
   return (
     <>
       <Layout></Layout>

4cfdbbbc9885829b012b101a2683b6c9e66b476e
Jonathan A <jonathanxl1@hotmail.com>
2025-01-04 01:01:02 -0500

    refactor: Set routes retrieve pages

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,6 +2,9 @@ import { PropsWithChildren } from "react";
 import Navbar from "../components/NavBar";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Login from "../pages/Login";
+import EditDelegations from "../pages/EditDelegations";
+import Home from "../pages/Home";
+import AboutMe from "../pages/AboutMe";
 
 interface Props extends PropsWithChildren {
   navbar: JSX.Element;
@@ -16,18 +19,23 @@ function Layout({ navbar, aside, main, footer, ...others }: Partial<Props>) {
       <BrowserRouter>
         <div className="w-full min-h-[600px] h-auto bg-slate-200 ">
           <nav className="w-full min-h-[40px] bg-gray-700">
-          {navbar ? navbar : <Navbar />}
-        </nav>
+            {navbar ? navbar : <Navbar />}
+          </nav>
 
-        <main className="w-full h-full min-h-[800px] flex">
+          <main className="w-full h-full min-h-[800px] flex">
             <Routes>
-              <Route path="/" element={<Login />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/aboutme" element={<AboutMe />} />
+
+              <Route path="/edit/:id" element={<EditDelegations />} />
+              <Route path="/create" element={<EditDelegations />} />
             </Routes>
-        </main>
-        <footer className="relative left-0 bottom-0 w-full min-h-[200px] bg-yellow-600">
-          {footer}
-        </footer>
-      </div>
+          </main>
+          <footer className="relative left-0 bottom-0 w-full min-h-[200px] bg-yellow-600">
+            {footer}
+          </footer>
+        </div>
       </BrowserRouter>
     </>
   );

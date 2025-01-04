1da46e6ff96d8674d9f76f8dc6f8f3ccfc95e020
Jonathan A <jonathanxl1@hotmail.com>
2025-01-04 00:40:57 -0500

    refactor: Aded service close session

--- src/storage/localStorage.storage.ts
+++ src/storage/localStorage.storage.ts
@@ -1,7 +1,13 @@
+const BEARER_TOKEN = "bearerToken";
+
 export function setBearerToken(token: string) {
-  localStorage.setItem("bearerToken", token);
+  localStorage.setItem(BEARER_TOKEN, token);
 }
 
 export function getBearerToken(): string | null {
-  return localStorage.getItem("bearerToken");
+  return localStorage.getItem(BEARER_TOKEN);
+}
+
+export function cleanBeareToken() {
+  localStorage.removeItem(BEARER_TOKEN);
 }
